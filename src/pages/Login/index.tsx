0357de427564d355d6e4af991446143e466dad96
RomanChen <stubstub8257@gmail.com>
2023-01-28 12:23:40 +0800

    style : modify style

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -17,37 +17,31 @@ const Login = () => {
               type="button"
               className="inline-block w-full rounded-lg bg-blue-500 py-2.5 text-center text-sm font-semibold text-white shadow-sm transition duration-200 hover:bg-blue-600 hover:shadow-md focus:bg-blue-700 focus:shadow-sm focus:ring-4 focus:ring-blue-500 focus:ring-opacity-50"
             >
-              <span className="mr-2 inline-block">Login</span>
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-                className="inline-block h-4 w-4"
-              >
-                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M17 8l4 4m0 0l-4 4m4-4H3" />
-              </svg>
-            </button>
+                <span className="mr-2">Login</span>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                  className="inline-block h-4 w-4"
+                >
+                  <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M17 8l4 4m0 0l-4 4m4-4H3" />
+                </svg>
+              </button>
           </div>
           <div className="p-5">
             <div className="grid grid-cols-3 gap-1">
               <button
                 type="button"
-                className="inline-block w-full rounded-lg border border-gray-200 py-2.5 text-center text-sm font-normal text-gray-500 shadow-sm transition duration-200 hover:shadow-md"
+                className="w-full rounded-lg border border-gray-200 py-2.5 text-center text-sm font-normal text-gray-500 shadow-sm transition duration-200 hover:shadow-md"
               >
-                MailUp
+                SignIn
               </button>
               <button
                 type="button"
-                className="inline-block w-full rounded-lg border border-gray-200 py-2.5 text-center text-sm font-normal text-gray-500 shadow-sm transition duration-200 hover:shadow-md"
+                className="w-full rounded-lg border border-gray-200 py-2.5 text-center text-sm font-normal text-gray-500 shadow-sm transition duration-200 hover:shadow-md"
               >
                 Google
-              </button>
-              <button
-                type="button"
-                className="inline-block w-full rounded-lg border border-gray-200 py-2.5 text-center text-sm font-normal text-gray-500 shadow-sm transition duration-200 hover:shadow-md"
-              >
-                Github
               </button>
             </div>
           </div>
@@ -69,7 +63,7 @@ const Login = () => {
                       d="M8 11V7a4 4 0 118 0m-4 8v2m-6 4h12a2 2 0 002-2v-6a2 2 0 00-2-2H6a2 2 0 00-2 2v6a2 2 0 002 2z"
                     />
                   </svg>
-                  <span className="ml-1 inline-block">Forgot Password</span>
+                  <span className="ml-1">Forgot Password</span>
                 </button>
               </div>
               <div className="whitespace-nowrap text-center sm:text-right">
@@ -88,7 +82,7 @@ const Login = () => {
                       d="M18.364 5.636l-3.536 3.536m0 5.656l3.536 3.536M9.172 9.172L5.636 5.636m3.536 9.192l-3.536 3.536M21 12a9 9 0 11-18 0 9 9 0 0118 0zm-5 0a4 4 0 11-8 0 4 4 0 018 0z"
                     />
                   </svg>
-                  <span className="ml-1 inline-block">Help</span>
+                  <span className="ml-1">Help</span>
                 </button>
               </div>
             </div>
@@ -107,7 +101,7 @@ const Login = () => {
                 >
                   <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M10 19l-7-7m0 0l7-7m-7 7h18" />
                 </svg>
-                <span className="ml-1 inline-block">Back to your-app.com</span>
+                <span className="ml-1">Back to your-app.com</span>
               </button>
             </div>
           </div>
