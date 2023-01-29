17f3c40139aa4008341874eb3d2834d35447894c
RomanChen <stubstub8257@gmail.com>
2023-01-29 18:27:09 +0800

    feat : hook function

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,22 +1,64 @@
+import useSignIn, { ISignIn } from '@/pages/Login/hooks/useSignIn';
+import useSignUp, { ISignUp } from '@/pages/Login/hooks/useSignUp';
+import { useForm } from 'react-hook-form';
+
 const Login = () => {
+  // https://hex-escape-room.herokuapp.com/api/user/signup
+  // https://hex-escape-room.herokuapp.com/api/user/signin
+
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm<ISignUp>();
+
+  const onSubmit = (data: ISignUp) => console.log(data);
+
+  const { trigger: signUpTrigger } = useSignUp();
+  //isMutating, data, error
+  const { trigger: signInTrigger } = useSignIn();
+
+  const signUp = () => {
+    signUpTrigger({
+      email: watch('email'),
+      password: watch('password'),
+      // email: 'lovef2e@hexschool.com',
+      // password: '12345678',
+    });
+  };
+
   return (
     <div className="flex min-h-screen flex-col justify-center bg-gray-100 sm:py-12">
       <div className="xs:p-0 mx-auto p-10 md:w-full md:max-w-md">
         <h1 className="mb-5 text-center text-2xl font-bold">Your Logo</h1>
         <div className="w-full divide-y divide-gray-200 rounded-lg bg-white shadow">
           <div className="px-5 py-7">
-            <label htmlFor="e-mail" className="block pb-1 text-sm font-semibold text-gray-600">
-              E-mail
-            </label>
-            <input id="e-mail" type="text" className="mt-1 mb-5 w-full rounded-lg border px-3 py-2 text-sm" />
-            <label id="password" htmlFor="2" className="block pb-1 text-sm font-semibold text-gray-600">
-              Password
-            </label>
-            <input id="password" type="text" className="mt-1 mb-5 w-full rounded-lg border px-3 py-2 text-sm" />
-            <button
-              type="button"
-              className="inline-block w-full rounded-lg bg-blue-500 py-2.5 text-center text-sm font-semibold text-white shadow-sm transition duration-200 hover:bg-blue-600 hover:shadow-md focus:bg-blue-700 focus:shadow-sm focus:ring-4 focus:ring-blue-500 focus:ring-opacity-50"
-            >
+            <form onSubmit={handleSubmit(onSubmit)}>
+              <label htmlFor="e-mail" className="block pb-1 text-sm font-semibold text-gray-600">
+                E-mail
+              </label>
+              <input
+                {...register('email', { required: true })}
+                id="e-mail"
+                type="text"
+                className="mt-1 mb-5 w-full rounded-lg border px-3 py-2 text-sm"
+              />
+              {/* {errors.email && <div className="text-gray-600">email 必填</div>} */}
+              <label id="password" htmlFor="password" className="block pb-1 text-sm font-semibold text-gray-600">
+                Password
+              </label>
+              <input
+                {...register('password', { required: true })}
+                id="password"
+                type="password"
+                className="mt-1 mb-5 w-full rounded-lg border px-3 py-2 text-sm"
+              />
+              {/* {errors.password && <div>password 必填</div>} */}
+              <button
+                type="submit"
+                className="w-full rounded-lg bg-blue-500 py-2.5 text-center text-sm font-semibold text-white shadow-sm transition duration-200 hover:bg-blue-600 hover:shadow-md focus:bg-blue-700 focus:shadow-sm focus:ring-4 focus:ring-blue-500 focus:ring-opacity-50"
+              >
                 <span className="mr-2">Login</span>
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
@@ -28,10 +70,12 @@ const Login = () => {
                   <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M17 8l4 4m0 0l-4 4m4-4H3" />
                 </svg>
               </button>
+            </form>
           </div>
           <div className="p-5">
             <div className="grid grid-cols-3 gap-1">
               <button
+                onClick={signUp}
                 type="button"
                 className="w-full rounded-lg border border-gray-200 py-2.5 text-center text-sm font-normal text-gray-500 shadow-sm transition duration-200 hover:shadow-md"
               >
