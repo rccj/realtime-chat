fd38ffdde5fb4dced15c0785834ba8bb3180ddc9
RomanChen <stubstub8257@gmail.com>
2023-01-30 22:24:00 +0800

    feat : remove api

--- vite.config.ts
+++ vite.config.ts
@@ -7,11 +7,6 @@ export default defineConfig({
   plugins: [react(), tsconfigPaths()],
   envDir: './env',
   server: {
-    proxy: {
-      '/api': {
-        target: 'https://hex-escape-room.herokuapp.com/',
-        changeOrigin: true,
-      },
-    },
+    open: true,
   },
 });
