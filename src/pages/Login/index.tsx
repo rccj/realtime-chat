acf0a3cd2367d313a271ae5552a4dd8d0bfb049b
RomanChen <stubstub8257@gmail.com>
2023-02-16 20:48:57 +0800

    style : remove common out

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -77,74 +77,11 @@ const Login = () => {
               <Button onClick={signUp} type="default">
                 SignUp
               </Button>
-              {/* <button
-                type="button"
-                className="w-full rounded-lg border border-gray-200 py-2.5 text-center text-sm font-normal text-gray-500 shadow-sm transition duration-200 hover:shadow-md"
-              >
-                Google
-              </button> */}
             </div>
           </div>
-          {/* <div className="py-5">
-            <div className="grid grid-cols-2 gap-1">
-              <div className="whitespace-nowrap text-center sm:text-left">
-                <button className="mx-5 cursor-pointer rounded-lg px-5 py-4 text-sm font-normal text-gray-500 ring-inset transition duration-200 hover:bg-gray-100 focus:bg-gray-200 focus:outline-none focus:ring-2 focus:ring-gray-400 focus:ring-opacity-50">
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    stroke="currentColor"
-                    className="inline-block h-4 w-4 align-text-top"
-                  >
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      strokeWidth="2"
-                      d="M8 11V7a4 4 0 118 0m-4 8v2m-6 4h12a2 2 0 002-2v-6a2 2 0 00-2-2H6a2 2 0 00-2 2v6a2 2 0 002 2z"
-                    />
-                  </svg>
-                  <span className="ml-1">Forgot Password</span>
-                </button>
-              </div>
-              <div className="whitespace-nowrap text-center sm:text-right">
-                <button className="mx-5 cursor-pointer rounded-lg px-5 py-4 text-sm font-normal text-gray-500 ring-inset transition duration-200 hover:bg-gray-100 focus:bg-gray-200 focus:outline-none focus:ring-2 focus:ring-gray-400 focus:ring-opacity-50">
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    stroke="currentColor"
-                    className="inline-block h-4 w-4 align-text-bottom	"
-                  >
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      strokeWidth="2"
-                      d="M18.364 5.636l-3.536 3.536m0 5.656l3.536 3.536M9.172 9.172L5.636 5.636m3.536 9.192l-3.536 3.536M21 12a9 9 0 11-18 0 9 9 0 0118 0zm-5 0a4 4 0 11-8 0 4 4 0 018 0z"
-                    />
-                  </svg>
-                  <span className="ml-1">Help</span>
-                </button>
-              </div>
-            </div>
-          </div> */}
         </div>
         <div className="py-5">
-          <div className="grid grid-cols-2 gap-1">
-            {/* <div className="whitespace-nowrap text-center sm:text-left">
-              <button className="mx-5 cursor-pointer rounded-lg px-5 py-4 text-sm font-normal text-gray-500 ring-inset transition duration-200 hover:bg-gray-200 focus:bg-gray-300 focus:outline-none focus:ring-2 focus:ring-gray-400 focus:ring-opacity-50">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke="currentColor"
-                  className="inline-block h-4 w-4 align-text-top"
-                >
-                  <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M10 19l-7-7m0 0l7-7m-7 7h18" />
-                </svg>
-                <span className="ml-1">Back to your-app.com</span>
-              </button>
-            </div> */}
-          </div>
+          <div className="grid grid-cols-2 gap-1"></div>
         </div>
       </div>
     </div>
