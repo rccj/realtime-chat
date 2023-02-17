52b97abea1e11f889130b8cd8e7f9f0b33a57e63
RomanChen <stubstub8257@gmail.com>
2023-02-17 21:37:35 +0800

    chore : init unplugin-icons

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import tsconfigPaths from 'vite-tsconfig-paths';
+import Icons from 'unplugin-icons/vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
+  plugins: [react(), tsconfigPaths(), Icons({ compiler: 'jsx', jsx: 'react' })],
   envDir: './env',
   server: {
     open: true,
