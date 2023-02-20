b16b262fa37e58d4cd9db5e8b2e7e8bc859bd784
RomanChen <stubstub8257@gmail.com>
2023-02-20 19:20:07 +0800

    feat : add send icon

--- src/pages/ChatRoom/index.tsx
+++ src/pages/ChatRoom/index.tsx
@@ -1,5 +1,6 @@
 import { useState, useRef } from 'react';
 import ChatMessage, { PositionEnum } from '@/pages/ChatRoom/ChatMessage';
+import IconMdiSend from '~icons/mdi/send';
 
 const defaultMessageInfos = [
   {
@@ -68,20 +69,7 @@ const ChatRoom = () => {
                   >
                     <span>Send</span>
                     <span className="ml-2">
-                      <svg
-                        className="-mt-px h-4 w-4 rotate-45 transform"
-                        fill="none"
-                        stroke="currentColor"
-                        viewBox="0 0 24 24"
-                        xmlns="http://www.w3.org/2000/svg"
-                      >
-                        <path
-                          strokeLinecap="round"
-                          strokeLinejoin="round"
-                          strokeWidth="2"
-                          d="M12 19l9 2-9-18-9 18 9-2zm0 0v-8"
-                        ></path>
-                      </svg>
+                      <IconMdiSend />
                     </span>
                   </button>
                 </div>
